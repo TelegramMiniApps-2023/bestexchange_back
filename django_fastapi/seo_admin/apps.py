150f1abeae8acfa983ab4f6bf9b53047be12b4fe
Vlad <yashenkov.v@yandex.ru>
2024-03-14 17:34:43 +0300

    fix: edit app name for seo_admin app

--- django_fastapi/seo_admin/apps.py
+++ django_fastapi/seo_admin/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class SeoAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SEO'
+    name = 'seo_admin'
+    verbose_name = 'SEO'
